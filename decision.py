22260b54f3eb41892af27ac6a96206e8db769aec
karl1ah <70944477+karl1ah@users.noreply.github.com>
2022-12-10 17:12:15 +0300

    asd

--- decision.py
+++ decision.py
@@ -6,24 +6,29 @@ from skimage import io
 
 img = []
 model = keras.models.load_model('./models/model_myCNN.h5')
-ad = "forrest"#input("resim adı ")
-try:
-    image1 = Image.open(ad+'.jpg')
-    asa= image1.size
-    if image1.size!=(250, 250):
-
+#ad = input("resim adı ")
+def isfire(path):
+    try:
+        image1 = Image.open(path)
         im2= image1.resize((250, 250))
-        im2.save(ad+'.jpg')
-    image = io.imread(ad+".jpg")
-    img.append(image)
-    x = np.array(img)
+        im2.save('x.jpg')
+        image = io.imread("x.jpg")
+        img.append(image)
+        x = np.array(img)
 
-    y = model.predict(x)
-    print(y)
-    y= np.argmax(y, axis=1)
-    if y ==0:
-        print("yangın yok")
-    elif y == 1:
-        print("yangın var")
-except FileNotFoundError :
-    print(" dosya bulunamadı hata!")
+        y = model.predict(x)
+        print(y)
+        y = np.argmax(y, axis=1)
+        
+        if y[len(y)-1] == 0:
+            str1 = 'yangın yok'
+            return str1
+            
+        elif y[len(y)-1]:
+            str1 = 'yangın var'
+            return str1   
+        
+    except FileNotFoundError :
+        str1 ='dosya bulunamadı hata!'
+        del y [0]
+        return str1
